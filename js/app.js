723e873aae4a8afff81beae40a19398bef2ae2cf
minhanhbg <vu.anh147852@gmail.com>
2022-02-20 00:12:32 +0700

    flix movie

--- js/app.js
+++ js/app.js
@@ -17,9 +17,47 @@ $(document).ready(()=>{
     $('#hero-carousel').owlCarousel({
         items:1,
         dots:false,
+        // loop: true, giúp trượt ngược được
         loop:true,
         nav:true,
         navText:navText,
+        autoplay:true,
         autoplayHoverPause:true
+    })
+    $('#top-movies-slide').owlCarousel({
+        items:2,
+        dots:false,
+        loop:true,
+        autoplay:false,
+        autoplayHoverPause:true,
+        responsive:{
+            500:{
+                items:3
+            },
+            1280:{
+                items:4
+            },
+            1600:{
+                items:6
+            }
+        }
+    })
+    $('#movies-slide').owlCarousel({
+        items:2,
+        dots:false,
+        nav:true,
+        navText:navText,
+        margin:15,
+        responsive:{
+            500:{
+                items:3
+            },
+            1280:{
+                items:4
+            },
+            1600:{
+                items:6
+            }
+        }
     })
 })
