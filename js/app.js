2d7d7c1c01da1d4b111bdb5ee2f75105324d6822
minhanhbg <vu.anh147852@gmail.com>
2022-02-19 17:04:11 +0700

    flix movie

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
+
+
+function act() {
+    let menu = document.querySelector("#hamburger-menu");
+    let navm = document.querySelector("#nav-menu");
+    menu.classList.toggle("active");
+    navm.classList.toggle("active");
+}
 $(document).ready(()=>{
-    $('#hamburger-menu').click(()=>{
-        $('#hamburger-menu').toggleClass('active')
-        $('#nav-menu').toggleClass('active')
+    // $('#hamburger-menu').click(()=>{
+    //     $('#hamburger-menu').toggleClass('active')
+    //     $('#nav-menu').toggleClass('active')
+    // })
+    // setting owl carousel
+    let navText =["<i class = 'bx bx-chevron-left'></i>",
+    "<i class = 'bx bx-chevron-right'></i>"]
+    $('#hero-carousel').owlCarousel({
+        items:1,
+        dots:false,
+        loop:true,
+        nav:true,
+        navText:navText,
+        autoplayHoverPause:true
     })
 })
